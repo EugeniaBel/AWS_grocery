89ff543516688cd783deef6ead937dd634d32537
EugeniaBel <belohay@gmail.com>
2025-09-20 01:45:21 +0200

    Final project cleanup before initial commit

---
+++ infrastruktur/main.tf
@@ -0,0 +1,70 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+data "aws_ami" "amzn-linux-2023-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amzn-linux-2023-ami.id
+  instance_type = "t2.micro"
+  key_name      = "grocerymate-key"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+}
+resource "aws_security_group" "web_sg" {
+  name_prefix = "web-sg-"
+  description = "Security group for web server"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "rds_sg" {
+  name_prefix = "rds-sg-"
+  description = "Security group for RDS database"
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_db_instance" "default" {
+  allocated_storage      = 10
+  db_name                = "grocerymate_db"
+  engine                 = "postgres"
+  engine_version         = "15.13"
+  instance_class         = "db.t3.micro"
+  username               = "grocery_user"
+  password               = "grocery_test"
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+}
