5754b5a9ce9a3cffd5512acb96002a14ee64d9ca
EugeniaBel <belohay@gmail.com>
2025-09-20 02:12:39 +0200

    Moved DB password to terraform.tfvars

--- infrastruktur/main.tf
+++ infrastruktur/main.tf
@@ -63,7 +63,7 @@ resource "aws_db_instance" "default" {
   engine_version         = "15.13"
   instance_class         = "db.t3.micro"
   username               = "grocery_user"
-  password               = "grocery_test"
+  password               = "var.db_password"
   skip_final_snapshot    = true
   publicly_accessible    = false
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
